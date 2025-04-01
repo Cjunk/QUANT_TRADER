c4cbecba55b3983d07fe5038b9cf256ba5e1bdf9
Jericho Sharman <jsharman@hotmail.com.au>
2025-04-01 18:35:08 +1100

    synching

--- temp_redis_listener.py
+++ temp_redis_listener.py
@@ -1,13 +1,25 @@
-# debug_redis_listener.py
 import redis
 import json
 import config.config_redis as config_redis
 
 r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 ps = r.pubsub()
-ps.subscribe(config_redis.SERVICE_STATUS_CHANNEL, "heartbeat_channel")
+ps.subscribe(
+    config_redis.SERVICE_STATUS_CHANNEL,
+    config_redis.HEARTBEAT_CHANNEL,
+    config_redis.COIN_FEED_AUTO,
+    config_redis.COIN_CHANNEL,
+    config_redis.TRADE_CHANNEL,
+    config_redis.ORDER_BOOK_UPDATES,
+    config_redis.KLINE_UPDATES
+)
 
 print("Listening...")
-for message in ps.listen():
-    if message["type"] == "message":
-        print(json.loads(message["data"]))
+try:
+    for message in ps.listen():
+        if message["type"] == "message":
+            print(json.loads(message["data"]))
+except KeyboardInterrupt:
+    print("\nInterrupted. Unsubscribing and exiting...")
+    ps.close()
+

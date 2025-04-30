04db6497021fb5105edb6a4af6d300e774f13c49
Jericho Sharman <jsharman@hotmail.com.au>
2025-05-01 06:29:46 +1000

    synch

--- bots/macro_metrics/runner.py
+++ bots/macro_metrics/runner.py
@@ -1,17 +1,17 @@
 import sys
 import time
 from utils.logger import setup_logger
-from macro_metrics_services_bot import Macro_metrics_services
+from macro_metrics_services_core import Macro_metrics_services
 import config.config_auto_macro_metrics_services as config
 
 if sys.prefix == sys.base_prefix:
     print("❌ Virtual environment is NOT activated! Please activate it first.")
     sys.exit(1)
 
-logger = setup_logger("MACRO_METRICS_SERVICES.log")
+#logger = setup_logger("MACRO_METRICS_SERVICES.log")
 
 if __name__ == "__main__":
-    open(config.LOG_FILENAME, 'w').close()
+    #open(config.LOG_FILENAME, 'w').close()
     logger.info("🚀 Starting MACRO_METRICS_SERVICES...")
     print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 🚀 Starting MACRO_METRICS_SERVICES...")
 
